e0a36e06d1a415f30956fea3725bfb4effabb2df
leokamwathi <leo.kamwathi@gmail.com>
2018-12-10 22:48:18 +0300

    Separated bot ligic and data from app.py. Added bot_chatdata and live_bot. I can define more bots and just call them in app.py. I might separate fb logic ??

---
+++ live_bot.py
@@ -0,0 +1,25 @@
+
+import re
+import random
+from bot_chatdata import BotChatData
+
+class LiveBot():
+    def __init__(self):
+        self.chatdata = BotChatData()
+        self.reflections = self.chatdata.reflections
+        self.psychobabble = self.chatdata.psychobabble
+
+    def reflect(self,fragment):
+        tokens = fragment.lower().split()
+        for i, token in enumerate(tokens):
+            if token in self.reflections:
+                tokens[i] = self.reflections[token]
+        return ' '.join(tokens)
+
+
+    def chat(self,statement):
+        for pattern, responses in self.psychobabble:
+            match = re.match(pattern, statement.rstrip(".!"))
+            if match:
+                response = random.choice(responses)
+                return response.format(*[self.reflect(g) for g in match.groups()])
